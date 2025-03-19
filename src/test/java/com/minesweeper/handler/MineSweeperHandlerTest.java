6c38b085c8023adb5fb4a1ef73dcbf2ae9d16733
Devanand Ukalkar <d.ukalkar@gmail.com>
2025-03-20 07:52:19 +0800

    MineSweeper implementation

--- src/test/java/com/minesweeper/handler/MineSweeperHandlerTest.java
+++ src/test/java/com/minesweeper/handler/MineSweeperHandlerTest.java
@@ -21,7 +21,7 @@ class MineSweeperHandlerTest {
     void shouldStartGameAndProcessValidMovesFromPlayer() {
         MineSweeperHandler mineSweeperHandler = new MineSweeperHandler(game) {
             int moveCount = 0;
-            String[] moves = {
+            final String[] moves = {
                     "A1", "A2", "A3", "A4",
                     "B1", "B2", "B3", "B4",
                     "C1", "C2", "C3", "C4",
